e85f050da9754af672d565da7e7cfd60d7656661
mlnmtt <imelon24@yahoo.com>
2017-05-30 18:55:26 +0200

    videogular fix

--- app/main.js
+++ app/main.js
@@ -28,7 +28,7 @@ angular.module('myApp',
                         default: ""
                     }
                 ],
-                theme: "bower_components/videogular-themes-default/videogular.css",
+                theme: "../lib/bower_components/videogular-themes-default/videogular.css",
                 plugins: {
                     poster: "http://www.videogular.com/assets/images/videogular.png"
                 }
