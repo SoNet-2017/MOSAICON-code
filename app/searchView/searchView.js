1af8a2f97e97ab3a4a233bc8ef5e79ca67015c6d
giul.cammarata <giuliano.cammarata@studenti.polito.it>
2017-07-10 17:25:56 +0200

    filter tab CSS tentativi errati

--- app/searchView/searchView.js
+++ app/searchView/searchView.js
@@ -23,12 +23,12 @@ angular.module('myApp.searchView', ['ngRoute'])
     }])
 
     .controller('searchCtrl', ['$scope', '$rootScope', 'currentAuth', '$firebaseAuth', '$location', 'eventsService', 'userChatService',
-        function($scope, $rootScope, currentAuth, $firebaseAuth, $location, eventService, userChatService) {
+        function($scope, $rootScope, currentAuth, $firebaseAuth, $location, eventsService, userChatService) {
         $scope.dati={};
         //set the variable that is used in the main template to show the active button
         $rootScope.dati.currentView = "search";
-        $scope.dati.events = eventService.getAllEvents();
-
+        $scope.dati.events = eventsService.getAllEvents();
+        /*
         $scope.filterClub = function () {
             events.style.display = block;
         };
@@ -37,7 +37,7 @@ angular.module('myApp.searchView', ['ngRoute'])
         };
         $scope.filterDJ = function () {
 
-        };
+        };*/
 
         $firebaseAuth().$onAuthStateChanged(function(firebaseUser) {
             if (firebaseUser) {
@@ -47,3 +47,24 @@ angular.module('myApp.searchView', ['ngRoute'])
             }
         });
     }]);
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
