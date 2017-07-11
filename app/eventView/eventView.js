3f22b06d3ed6cc05292b3e5d16fef7ebdc8f8c74
mlnmtt <imelon24@yahoo.com>
2017-07-11 18:23:02 +0200

    passport design

--- app/eventView/eventView.js
+++ app/eventView/eventView.js
@@ -216,9 +216,7 @@ angular.module('myApp.eventView',
 
             this.config = {
                 sources: [
-                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"}
                 ],
                 theme: "../../lib/bower_components/videogular-themes-default/videogular.css",
 
